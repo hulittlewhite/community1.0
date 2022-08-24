e8c6c7910c0ec088f521b8b68372b6db73dfc946
hutianle <hutianle@jieshitech.com>
2022-08-24 16:21:18 +0800

    chapter 3

--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -45,8 +45,8 @@ public class DiscussPostService {
         return discussPostMapper.selectDiscussPostById(id);
     }
 
-//    public int updateCommentCount(int id, int commentCount) {
-//        return discussPostMapper.updateCommentCount(id, commentCount);
-//    }
+    public int updateCommentCount(int id, int commentCount) {
+        return discussPostMapper.updateCommentCount(id, commentCount);
+    }
 
 }
